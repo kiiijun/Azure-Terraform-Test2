51d345f61333326fb0b4be0f0b99853d2476e0e1
Kijun Nam <snrm123@naver.com>
2023-04-26 17:33:31 +0900

    root 모듈 구현 진행중

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.53.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+module "hub_network" {
+  source = "./modules/virtual_network"
+
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  type = string
+}
+variable "location" {
+  default = "Korea Central"
+  type    = string
+}
+variable "tags" {
+  default = {}
+}
