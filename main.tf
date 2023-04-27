8e35bba36c64e7dc0cbea52373bb894cffad51f8
Kijun Nam <snrm123@naver.com>
2023-04-27 16:57:23 +0900

    Vnet 및 피어링 배포

--- main.tf
+++ main.tf
@@ -19,6 +19,49 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "hub_network" {
-  source = "./modules/virtual_network"
+  source              = "./modules/virtual_network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  vnet_name           = var.hub_vnet_name
+  address_space       = var.hub_address_space
+  tags                = var.tags
 
+  subnets = [
+    {
+      name : "AzureFirewallSubnet"
+      address_prefixes : var.hub_firewall_subnet_address_prefix
+    },
+    {
+      name : "GatewaySubnet"
+      address_prefixes : var.hub_gateway_subnet_address_prefix
+    }
+  ]
+}
+
+module "spoke_network" {
+  source              = "./modules/virtual_network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  vnet_name           = var.spoke_vnet_name
+  address_space       = var.spoke_address_space
+  tags                = var.tags
+
+  subnets = [
+    {
+      name : "aks-subnet"
+      address_prefixes : var.spoke_aks_subnet_address_prefix
+    }
+  ]
+}
+
+module "vnet_peering" {
+  source              = "./modules/virtual_network_peering"
+  vnet_1_name         = var.hub_vnet_name
+  vnet_1_id           = module.hub_network.vnet_id
+  vnet_1_rg           = azurerm_resource_group.rg.name
+  vnet_2_name         = var.spoke_vnet_name
+  vnet_2_id           = module.spoke_network.vnet_id
+  vnet_2_rg           = azurerm_resource_group.rg.name
+  peering_name_1_to_2 = "${var.hub_vnet_name}To${var.spoke_vnet_name}"
+  peering_name_2_to_1 = "${var.spoke_vnet_name}To${var.hub_vnet_name}"
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
+//리소스 그룹 변수
 variable "resource_group_name" {
-  type = string
+  default = "test-rg"
+  type    = string
 }
 variable "location" {
   default = "Korea Central"
@@ -7,4 +9,36 @@ variable "location" {
 }
 variable "tags" {
   default = {}
+}
+
+//허브 vnet 변수
+variable "hub_vnet_name" {
+  default = "hub-vnet"
+  type    = string
+}
+variable "hub_address_space" {
+  default = ["10.1.0.0/16"]
+  type    = list(string)
+}
+variable "hub_firewall_subnet_address_prefix" {
+  default = ["10.1.0.0/24"]
+  type    = list(string)
+}
+variable "hub_gateway_subnet_address_prefix" {
+  default = ["10.1.1.0/24"]
+  type    = list(string)
+}
+
+//스포크 vnet 변수
+variable "spoke_vnet_name" {
+  default = "spoke-vnet"
+  type    = string
+}
+variable "spoke_address_space" {
+  default = ["10.2.0.0/16"]
+  type    = list(string)
+}
+variable "spoke_aks_subnet_address_prefix" {
+  default = ["10.2.0.0/24"]
+  type    = list(string)
 }
