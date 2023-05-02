56d0570d0b075fc0049a1ab8e1a1ceeed220acb1
Kijun Nam <snrm123@naver.com>
2023-05-02 10:51:23 +0900

    AKS 관련 코드 삭제

--- main.tf
+++ main.tf
@@ -52,12 +52,8 @@ module "spoke_network" {
 
   subnets = [
     {
-      name : var.default_node_pool_subnet_name
-      address_prefixes : var.default_node_pool_subnet_address_prefix
-    },
-    {
-      name : var.additional_node_pool_subnet_name
-      address_prefixes : var.additional_node_pool_subnet_address_prefix
+      name : var.aks_subnet_name
+      address_prefixes : var.aks_subnet_address_prefix
     },
     {
       name : var.vm_subnet_name
@@ -89,39 +85,4 @@ module "firewall" {
   sku_tier            = var.firewall_sku_tier
   pip_name            = "${var.firewall_name}-pip"
   subnet_id           = module.hub_network.subnet_ids["AzureFirewallSubnet"]
-  # log_analytics_workspace_id   = module.log_analytics_workspace.id
-  # log_analytics_retention_days = var.log_analytics_retention_days
-}
-
-module "aks_cluster" {
-  source                  = "./modules/aks"
-  name                    = var.aks_cluster_name
-  location                = var.location
-  resource_group_name     = azurerm_resource_group.rg["${var.resource_group_count}" - 1].name
-  resource_group_id       = azurerm_resource_group.rg["${var.resource_group_count}" - 1].id
-  kubernetes_version      = var.kubernetes_version
-  dns_prefix              = lower(var.aks_cluster_name)
-  private_cluster_enabled = true
-  # automatic_channel_upgrade                = var.automatic_channel_upgrade
-  sku_tier                                 = var.sku_tier
-  default_node_pool_name                   = var.default_node_pool_name
-  default_node_pool_vm_size                = var.default_node_pool_vm_size
-  vnet_subnet_id                           = module.spoke_network.subnet_ids[var.default_node_pool_subnet_name]
-  default_node_pool_availability_zones     = var.default_node_pool_availability_zones
-  default_node_pool_node_labels            = var.default_node_pool_node_labels
-  default_node_pool_node_taints            = var.default_node_pool_node_taints
-  default_node_pool_enable_auto_scaling    = var.default_node_pool_enable_auto_scaling
-  default_node_pool_enable_host_encryption = var.default_node_pool_enable_host_encryption
-  default_node_pool_enable_node_public_ip  = var.default_node_pool_enable_node_public_ip
-  default_node_pool_max_pods               = var.default_node_pool_max_pods
-  default_node_pool_max_count              = var.default_node_pool_max_count
-  default_node_pool_min_count              = var.default_node_pool_min_count
-  default_node_pool_node_count             = var.default_node_pool_node_count
-  default_node_pool_os_disk_type           = var.default_node_pool_os_disk_type
-  tags                                     = var.tags
-  network_dns_service_ip                   = var.network_dns_service_ip
-  network_plugin                           = var.network_plugin
-  outbound_type                            = "loadBalancer"
-  network_service_cidr                     = var.network_service_cidr
-  tenant_id                                = data.azurerm_client_config.current.tenant_id
 }
--- variables.tf
+++ variables.tf
@@ -42,28 +42,22 @@ variable "spoke_address_space" {
   default = ["10.2.0.0/16"]
   type    = list(string)
 }
-variable "default_node_pool_subnet_name" {
-  default = "system-subnet"
+
+variable "aks_subnet_name" {
+  default = "aks-subnet"
   type    = string
 }
-variable "default_node_pool_subnet_address_prefix" {
-  default = ["10.2.1.0/24"]
+variable "aks_subnet_address_prefix" {
+  default = ["10.2.0.0/24"]
   type    = list(string)
 }
-variable "additional_node_pool_subnet_name" {
-  default = "user-subnet"
-  type    = string
-}
-variable "additional_node_pool_subnet_address_prefix" {
-  default = ["10.2.2.0/24"]
-  type    = list(string)
-}
+
 variable "vm_subnet_name" {
   default = "vm-subnet"
   type    = string
 }
 variable "vm_subnet_address_prefix" {
-  default = ["10.2.3.0/24"]
+  default = ["10.2.1.0/24"]
   type    = list(string)
 }
 
@@ -87,89 +81,4 @@ variable "firewall_sku_name" {
 variable "firewall_sku_tier" {
   default = "Standard"
   type    = string
-}
-
-//aks
-variable "aks_cluster_name" {
-  default = "spoke-aks"
-  type    = string
-}
-variable "kubernetes_version" {
-  default = "1.25.6"
-  type    = string
-}
-variable "sku_tier" {
-  default = "Free"
-  type    = string
-}
-
-
-//default node pool 변수
-variable "default_node_pool_name" {
-  default = "system"
-  type    = string
-}
-variable "default_node_pool_vm_size" {
-  default = "Standard_B2s"
-  type    = string
-}
-variable "default_node_pool_availability_zones" {
-  default = []
-  type    = list(string)
-}
-variable "default_node_pool_node_labels" {
-  default = {}
-  type    = map(any)
-}
-variable "default_node_pool_node_taints" {
-  default = []
-  type    = list(string)
-}
-variable "default_node_pool_enable_auto_scaling" {
-  default = true
-  type    = bool
-}
-variable "default_node_pool_enable_host_encryption" {
-  default = false
-  type    = bool
-}
-variable "default_node_pool_enable_node_public_ip" {
-  default = false
-  type    = bool
-}
-variable "default_node_pool_max_pods" {
-  default = 30
-  type    = number
-}
-variable "default_node_pool_max_count" {
-  type    = number
-  default = 10
-}
-variable "default_node_pool_min_count" {
-  type    = number
-  default = 3
-}
-variable "default_node_pool_node_count" {
-  type    = number
-  default = 3
-}
-variable "default_node_pool_os_disk_type" {
-  type    = string
-  default = "Managed"
-}
-variable "network_dns_service_ip" {
-  default = "10.2.0.10"
-  type    = string
-}
-variable "network_plugin" {
-  default = "azure"
-  type    = string
-}
-variable "network_service_cidr" {
-  default = "10.2.0.0/24"
-  type    = string
-}
-variable "role_based_access_control_enabled" {
-  default = false
-  type    = bool
 }
