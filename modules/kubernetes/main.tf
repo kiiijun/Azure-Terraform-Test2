1bf69a9b3a86f6170e3b4a11c1b7f113bbf15801
Kijun Nam <snrm123@naver.com>
2023-05-04 09:32:38 +0900

    쿠버네티스 모듈 오토 스케일링 옵션 추가

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -8,16 +8,27 @@ terraform {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  dns_prefix          = var.dns_prefix
-  tags                = var.tags
+  name                    = var.cluster_name
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  dns_prefix              = var.dns_prefix
+  tags                    = var.tags
+  private_cluster_enabled = var.private_cluster_enabled
+  sku_tier                = var.sku_tier
 
   default_node_pool {
-    name       = var.default_node_pool_name
-    node_count = var.default_node_pool_node_count
-    vm_size    = var.default_node_pool_vm_size
+    vnet_subnet_id      = var.vnet_subnet_id
+    name                = var.default_node_pool_name
+    node_count          = var.default_node_pool_node_count
+    vm_size             = var.default_node_pool_vm_size
+    zones               = var.zones
+    enable_auto_scaling = var.default_node_pool_enable_auto_scaling
+    min_count           = var.default_node_pool_min_count
+    max_count           = var.default_node_pool_max_count
+    max_pods            = var.default_node_pool_max_pods
+  }
+  network_profile {
+    network_plugin = var.network_plugin
   }
 
   identity {
--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -23,10 +23,45 @@ variable "default_node_pool_vm_size" {
   type    = string
   default = "Standard_B2s"
 }
+variable "zones" {
+  default = ["1", "2", "3"]
+  type    = list(string)
+}
+variable "default_node_pool_enable_auto_scaling" {
+  default = true
+  type    = bool
+}
+variable "default_node_pool_min_count" {
+  default = 1
+  type    = number
+}
+variable "default_node_pool_max_count" {
+  default = 3
+  type    = number
+}
+variable "default_node_pool_max_pods" {
+  default = 30
+  type    = number
+}
 variable "identity" {
   type    = string
   default = "SystemAssigned"
 }
 variable "tags" {
   default = {}
+}
+variable "vnet_subnet_id" {
+  type = string
+}
+variable "network_plugin" {
+  default = "azure"
+  type    = string
+}
+variable "private_cluster_enabled" {
+  default = true
+  type    = bool
+}
+variable "sku_tier" {
+  default = "Free"
+  type    = string
 }
